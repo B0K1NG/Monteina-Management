e64a6447298101fc29f2fb13d3c116a237300ce9
B0K1NG <dominykaspavlijus@gmail.com>
2025-04-25 21:50:46 +0300

    fix: Correct API endpoint URLs for fetching vehicle makes and models

--- client/src/api/vehicles.ts
+++ client/src/api/vehicles.ts
@@ -3,11 +3,11 @@ import axios from 'axios';
 const BASE_URL = 'https://vpic.nhtsa.dot.gov/api';
 
 export const getAllMakes = async () => {
-    const response = await axios.get(`${BASE_URL}/vehiclesGetAllMakes?format=json`);
+    const response = await axios.get(`${BASE_URL}/vehicles/getallmakes?format=json`);
     return response.data.Results;
 };
 
 export const getModelsForMake = async (make: string) => {
-    const response = await axios.get(`${BASE_URL}/vehicles/GetModelsForMake/${make}?format=json`);
+    const response = await axios.get(`${BASE_URL}/vehicles/getmodelsformake/${make}?format=json`);
     return response.data.Results;
 };
