ca0d2b2dba5a9049bffafdc75e8834782a0d83ed
B0K1NG <dominykaspavlijus@gmail.com>
2025-05-26 14:20:22 +0300

    fix: ensure BASE_URL defaults to localhost if backend URL is not set

--- client/src/api/vehicles.ts
+++ client/src/api/vehicles.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = `${import.meta.env.VITE_BACKEND_URL.replace(/\/$/, '')}/api/carquery`;
+const BASE_URL = `${(import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000').replace(/\/$/, '')}/api/carquery`;
 
 export const getAllMakes = async () => {
     const response = await axios.get(`${BASE_URL}?callback=?&cmd=getMakes`);
